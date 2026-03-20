a4fc77b25f0cb6a25db14f5f428ce6fb2532a347
Vanchha16 <157358016+Vanchha16@users.noreply.github.com>
2026-03-20 20:04:32 +0700

    respon

--- resources/views/backend/page/borrows/overdue.blade.php
+++ resources/views/backend/page/borrows/overdue.blade.php
@@ -225,7 +225,7 @@
 
                                 <!-- Update Modal -->
                                 <div class="modal fade" id="{{ $modalId }}" tabindex="-1" aria-labelledby="{{ $modalId }}Label" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-dialog modal-lg modal-dialog-centered">
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <h5 class="modal-title" id="{{ $modalId }}Label">
