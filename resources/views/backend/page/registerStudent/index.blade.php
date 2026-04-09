5868715702e5554e99aae58aa76da59b48604142
Vanchha16 <157358016+Vanchha16@users.noreply.github.com>
2026-04-09 13:12:40 +0700

    m

--- resources/views/backend/page/registerStudent/index.blade.php
+++ resources/views/backend/page/registerStudent/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Student Registration</title>
-
+  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/photo_2024-05-27_08-46-50.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
     <link rel="preconnect" href="https://googleapis.com">
